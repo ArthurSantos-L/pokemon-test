3f0f03eb2c3f7beb1c7f7e6ac11e35b7b1794d3c
ArthurSantos-L <arthur.santos@gentrop.com>
2022-10-24 18:58:55 -0300

    chore: first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+
+const PORT = 5000
+
+const basePokemonURL = (pokemonName) => `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
+app.get("/pokemon/:name", async  (req,res) =>{
+  const pokemonName = req.params.name
+  const response = await axios.get(basePokemonURL(pokemonName))
+  const {id, name, abilities} = response.data
+  res.json({id,name, abilities})
+})
+
+app.get("/", (req, res)=>{
+  res.json("DUMMY API")
+})
+
+app.listen(PORT, ()=>{
+  console.log(`server is running on ${PORT}`)
+})
