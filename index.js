dceb9fa03afbf452a714d3e3a0d5a4e12ee267e3
ArthurSantos-L <arthur.santos@gentrop.com>
2022-10-27 10:28:45 -0300

    fix: remove crashing bug from not having try catch

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ app.use('/docs', swaggerServe)
 app.get('/docs', swaggerSetup(docs))
 
 app.get("/pokemon/:name", async  (req,res) =>{
-
-  const pokemonName = req.params.name
-  const response = await axios.get(basePokemonURL(pokemonName))
-  const {id, name, abilities} = response.data
-  res.json({id,name, abilities})
+  try{
+    const pokemonName = req.params.name
+    const response = await axios.get(basePokemonURL(pokemonName))
+    const {id, name, abilities} = response.data
+    res.json({id,name, abilities})
+  }catch(error){
+    res.status(500)
+    res.json({message: `Error!\n ${JSON.stringify(error)}`})
+  }
 })
 
 app.get("/", (req, res)=>{
